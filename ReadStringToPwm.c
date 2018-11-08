d03d503118fb8aad8265f0188e2675bb21a873cf
mee427 <44198702+mee427@users.noreply.github.com>
2018-11-08 16:53:27 +0300

    Update ReadStringToPwm.c

--- ReadStringToPwm.c
+++ ReadStringToPwm.c
@@ -1,6 +1,12 @@
-#include <pid_control.h>
+#include <16F877A.h>
 #include <stdlib.h>
-#use rs232(baud=9600,xmit=PIN_C6,rcv=PIN_C7)
+
+#FUSES NOWDT                 	//No Watch Dog Timer
+#FUSES NOBROWNOUT            	//No brownout reset
+#FUSES NOLVP                 	//No low voltage prgming, B3(PIC16) or B5(PIC18) used for I/O
+
+#use delay(crystal=20000000)
+#use rs232 (baud=9600,xmit=PIN_C6, rcv=PIN_C7, parity=N, stop=1) 
 
 int counter = 0;
 char strInput[4];
